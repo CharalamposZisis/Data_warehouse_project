d91413e39132ca58597a47f7056c4bab0dd2625f
Mpampis1312 <charalamposzeses282@gmail.com>
2025-09-17 20:21:16 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+/*
+===================================================
+Create schemas
+==================================================
+Cript Purpose:
+  This script sets up schemas within the database: 'bronze', 'silver', and 'gold'
+*/
+
+create schema bronze;
+
+create schema silver;
+
+create schema gold;
+
