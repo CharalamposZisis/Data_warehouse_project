12f7c3994b9c02bee8d7e068a0b47956694e4c92
Mpampis1312 <charalamposzeses282@gmail.com>
2025-09-18 11:22:06 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -6,9 +6,32 @@ Cript Purpose:
   This script sets up schemas within the database: 'bronze', 'silver', and 'gold'
 */
 
-create schema bronze;
+-- CREATE DATABASE 'DATAWAREHOUSE
 
-create schema silver;
+USE master;
+GO
 
-create schema gold;
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
